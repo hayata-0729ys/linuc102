b143d529ead48474b8cf080f25842a892fc0ad3e
hayata-0729ys <ys2907ha@gmail.com>
2026-06-05 00:32:17 +0900

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,57 @@
+const questions = [
+
+{
+category:"systemd",
+
+question:"systemdでサービスを起動するコマンドは？",
+
+choices:[
+"systemctl start",
+"service up",
+"runsvc",
+"systemd run"
+],
+
+answer:0,
+
+explanation:
+"systemctl start サービス名 を利用する。"
+},
+
+{
+category:"ssh",
+
+question:"SSHの標準ポート番号は？",
+
+choices:[
+"22",
+"21",
+"80",
+"443"
+],
+
+answer:0,
+
+explanation:
+"SSHはTCP22番ポートを利用する。"
+},
+
+{
+category:"filesystem",
+
+question:"パーミッション変更コマンドは？",
+
+choices:[
+"chmod",
+"chown",
+"umask",
+"passwd"
+],
+
+answer:0,
+
+explanation:
+"chmodはアクセス権を変更する。"
+}
+
+];
